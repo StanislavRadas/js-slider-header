a19fe8cdb001f04ca18e97539efe41788f5db115
StanislavRadas <stasradysh@gmail.com>
2024-04-03 17:33:23 +0200

    js-slider-header

--- src/container/index/index.js
+++ src/container/index/index.js
@@ -1,3 +1,87 @@
-import '../../script/test'
+class Slider {
+    static #content = null
+    static #left = null
+    static #right = null
+    static #count = 1
+    static #max = null
 
-console.log('container')
+    static init = () => {
+        this.#content = document.querySelector('.slider__content')
+        this.#left = document.querySelector('.button--left')
+        this.#right = document.querySelector('.button--right')
+        this.#max = this.#content.childElementCount
+        this.#left.onclick = () => this.#slide('left')
+        this.#right.onclick = () => this.#slide('right')
+    }
+
+    static #slide = (side) => {
+        const offsetWidth = this.#content.offsetWidth
+        const scrollLeft = this.#content.scrollLeft
+        const scrollWidth = this.#content.scrollWidth
+
+        let scroll = 0
+
+        if (side === 'left') {
+            if (this.#count === 1) {
+                this.#count = this.#max 
+                scroll = (this.#count - 1) * offsetWidth
+            } else {
+                this.#count -= 1
+                scroll = (this.#count - 1) * offsetWidth
+            }
+        }
+
+        if (side === 'right') {
+            if (this.#count === this.#max) {
+                this.#count = 1
+                scroll = 0
+            } else {
+                this.#count += 1
+                scroll = (this.#count - 1) * offsetWidth
+            }
+        }
+
+        this.#content.scrollTo({
+            top: 0,
+            left: scroll,
+            behavior: 'smooth',
+        })
+    } 
+}
+
+Slider.init()
+
+class Header {
+    static #height = null
+    static #wrapper = null
+    static #button = null
+    static #isOpen = false
+
+    static init = () => {
+        this.#height = document.querySelector('.menu',
+        ).offsetHeight
+        this.#wrapper = document.querySelector('.menu__wrapper',
+        )
+        this.#button = document.querySelector('.nav__button')
+        this.#button.onclick = this.#toggle
+    }
+
+    static #toggle = () => {
+        if (this.#isOpen) {
+            this.#button.classList.replace(
+                'nav__button--close',
+                'nav__button--open'
+            )
+            this.#wrapper.style.height = 0
+        } else {
+             this.#button.classList.replace(
+                'nav__button--open',
+                'nav__button--close'
+             )
+            this.#wrapper.style.height = `${this.#wrapper.scrollHeight}px`
+        }
+        this.#isOpen = !this.#isOpen
+    }
+}
+
+Header.init()
